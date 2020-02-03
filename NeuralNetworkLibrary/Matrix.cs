cf28bd7f1281b4d6044fe48658787323d7ee9380
pastencopy <gwangsuleee@gmail.com>
2020-02-04 04:24:05 +0900

    first commit

---
+++ NeuralNetworkLibrary/Matrix.cs
@@ -0,0 +1,202 @@
+﻿/*
+ * Matrix.cs
+ * 
+ * N by N 행렬 구현
+ *  
+ * 2020-02-03 Coded by GwangSu Lee 
+ * 
+ * 
+ * Original Reference : https://github.com/CodingTrain/Toy-Neural-Network-JS/blob/master/lib/matrix.js
+
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace NeuralNetworkLibrary
+{
+    class Matrix
+    {
+        //행렬 데이터
+        public int rows;
+        public int cols;
+        public double[,] data;
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[this.rows, this.cols];
+        }
+
+        public Matrix Copy()
+        {
+            Matrix result = new Matrix(this.rows, this.cols);
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    result.data[i, j] = this.data[i, j];
+                }
+            }
+            return result;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                Console.Write("[");
+                for (int j = 0; j < this.cols; j++)
+                {
+                    Console.Write(string.Format("{0},", this.data[i, j]));
+                }
+                Console.Write("]\n");
+            }
+        }
+
+        public static Matrix ConvertFromArray(double[] arr)
+        {
+            return new Matrix(arr.Length, 1).map((x, i) => arr[i]);
+        }
+
+        public static Matrix Substract(Matrix a, Matrix b)
+        {
+            if (a.rows != b.rows || a.cols != b.cols)
+            {
+                Console.Error.WriteLine("Columns ans Rows Not Match");
+                return null;
+            }
+
+            return new Matrix(a.rows, a.cols).map((x, i, j) => a.data[i, j] - b.data[i, j]);
+        }
+
+        public static Matrix Transpose(Matrix m)
+        {
+            return new Matrix(m.cols, m.rows).map((x, i, j) => m.data[j, i]);
+        }
+
+        //행렬 곱셈
+        public static Matrix Multiply(Matrix a, Matrix b)
+        {
+            if (a.cols != b.rows)
+            {
+                Console.Error.WriteLine("Cols 와 Rows가 같아야 곱할 수 있습니다. a x b ");
+                return null;
+            }
+
+            return new Matrix(a.rows, b.cols).map((x, i, j) =>
+            {
+                double sum = 0;
+                for (int k = 0; k < a.cols; k++)
+                {
+                    sum += a.data[i, k] * b.data[k, j];
+                }
+                return sum;
+            });
+        }
+
+        //행렬에 전부 매핑하는 함수
+        public static Matrix map(Matrix m, Func<double, int, int, double> func)
+        {
+            return new Matrix(m.rows, m.cols).map((x, i, j) => func(m.data[i, j], i, j));
+        }
+
+        //행렬 구성분 상수배
+        public Matrix Multiply(Matrix m)
+        {
+            if (this.rows != m.rows || this.cols != m.cols)
+            {
+                Console.Error.WriteLine("Columns 와 Rows 는 같아야 상수배 곱할 수 있습니다.");
+                return null;
+            }
+
+            return this.map((x, i, j) => x * m.data[i, j]);
+        }
+        public Matrix Multiply(double n)
+        {
+            return this.map(x => x * n);
+        }
+
+        public double[] ToArray()
+        {
+            double[] arr = new double[rows * cols];
+            int index = 0;
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    arr[index++] = this.data[i, j];
+                }
+            }
+            return arr;
+        }
+
+        public Matrix Add(Matrix m)
+        {
+            if (this.rows != m.rows || this.cols != m.cols)
+            {
+                Console.Error.WriteLine("Columns Rows 는 같아야 합니다.");
+                return null;
+            }
+            return this.map((x, i, j) => x + m.data[i, j]);
+        }
+
+        public Matrix Add(double n)
+        {
+            return this.map(x => x + n);
+        }
+
+        public Matrix Randomize()
+        {
+            Random rnd = new Random();
+            return this.map(x => (rnd.NextDouble() * 2 - 1));
+        }
+
+        public Matrix map(Func<double, double> func)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.data[i, j] = func(this.data[i, j]);
+                }
+            }
+            return this;
+        }
+
+        public Matrix map(Func<double, int, double> func)
+        {
+            int index = 0;
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    this.data[i, j] = func(this.data[i, j], index++);
+                }
+            }
+            return this;
+        }
+
+        public Matrix map(Func<double, int, int, double> func)
+        {
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    double val = this.data[i, j];
+                    this.data[i, j] = func(val, i, j);
+                }
+            }
+            return this;
+        }
+
+    }
+}
---
+++ NeuralNetworkLibrary/NeuralNetwork.cs
@@ -0,0 +1,183 @@
+﻿/*
+ *   NeuralNetworkLibrary
+ * 
+ *   N x N 배열에 Hidden레이어 1개로 구성된 단순한 신경망 구조
+ * 
+ *   2020-02-03 Coded by GwangSu Lee 
+ * 
+ * 
+ * Original Reference : https://github.com/CodingTrain/Toy-Neural-Network-JS/blob/master/lib/nn.js
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkLibrary
+{
+    class ActivationFunction
+    {
+        public Func<double, int, int, double> func;
+        public Func<double, int, int, double> dfunc;
+        public ActivationFunction(Func<double, int, int, double> func, Func<double, int, int, double> dfunc)
+        {
+            this.func = func;
+            this.dfunc = dfunc;
+        }
+
+        public static ActivationFunction SIGMOID = new ActivationFunction(
+            (x, i, j) => 1 / (1 + Math.Exp(-x)),
+            (y, i, j) => y * (1 - y)
+        );
+
+        public static ActivationFunction TANH = new ActivationFunction(
+            (x, i, j) => Math.Tanh(x),
+            (y, i, j) => 1 - (y * y)
+        );
+    }
+
+    class NeuralNetwork
+    {
+        public int input;
+        public int hidden;
+        public int output;
+
+        public Matrix bias_h;
+        public Matrix bias_o;
+
+        public Matrix weights_ih;
+        public Matrix weights_ho;
+
+        public double learning_rate;
+
+        public ActivationFunction activation_function;
+
+        public NeuralNetwork(NeuralNetwork dup)
+        {
+            this.input = dup.input;
+            this.hidden = dup.hidden;
+            this.output = dup.output;
+
+            this.weights_ho = dup.weights_ho.Copy();
+            this.weights_ih = dup.weights_ih.Copy();
+
+            this.bias_h = dup.bias_h.Copy();
+            this.bias_o = dup.bias_o.Copy();
+
+            this.Preload();
+        }
+        public NeuralNetwork(int input, int hidden, int output)
+        {
+            this.input = input;
+            this.hidden = hidden;
+            this.output = output;
+
+            this.weights_ih = new Matrix(this.hidden, this.input);
+            this.weights_ho = new Matrix(this.output, this.hidden);
+            this.weights_ih.Randomize();
+            this.weights_ho.Randomize();
+
+            this.bias_h = new Matrix(this.hidden, 1);
+            this.bias_o = new Matrix(this.output, 1);
+            this.bias_h.Randomize();
+            this.bias_o.Randomize();
+
+            this.Preload();
+        }
+
+        private void Preload()
+        {
+            this.SetLearningRate();
+            this.SetActivationFunction(ActivationFunction.SIGMOID);
+        }
+
+        public void SetLearningRate(double rate = 0.1)
+        {
+            this.learning_rate = rate;
+        }
+
+        public void SetActivationFunction(ActivationFunction func)
+        {
+            this.activation_function = func;
+        }
+
+        public double[] Predict(double[] input_array)
+        {
+            Matrix inputs = Matrix.ConvertFromArray(input_array);
+            Matrix hidden = Matrix.Multiply(this.weights_ih, inputs);
+            hidden.Add(this.bias_h);
+
+            hidden.map(this.activation_function.func);
+
+            Matrix output = Matrix.Multiply(this.weights_ho, hidden);
+            output.Add(this.bias_o);
+            output.map(this.activation_function.func);
+
+            return output.ToArray();
+        }
+
+        public void Train(double[] input_array, double[] target_array)
+        {
+            Matrix inputs = Matrix.ConvertFromArray(input_array);
+            Matrix hidden = Matrix.Multiply(this.weights_ih, inputs);
+            hidden.Add(this.bias_h);
+            hidden.map(this.activation_function.func);
+
+            //출력 만들기
+            Matrix outputs = Matrix.Multiply(this.weights_ho, hidden);
+            outputs.Add(this.bias_o);
+            outputs.map(this.activation_function.func);
+
+            Matrix targets = Matrix.ConvertFromArray(target_array);
+
+            //1. 출력 오차 계산
+            // errors = targets - outputs
+            Matrix output_errors = Matrix.Substract(targets, outputs);
+
+            //gradient 계산
+            Matrix gradients = Matrix.map(outputs, this.activation_function.dfunc);
+            gradients.Multiply(output_errors);
+            gradients.Multiply(this.learning_rate);
+
+            //DELTA계산
+            Matrix hidden_T = Matrix.Transpose(hidden);
+            Matrix weight_ho_deltas = Matrix.Multiply(gradients, hidden_T);
+
+            //가중치 보정
+            this.weights_ho.Add(weight_ho_deltas);
+            this.bias_o.Add(gradients);
+
+            //2. hidden 오차 계산
+            Matrix weight_ho_T = Matrix.Transpose(this.weights_ho);
+            Matrix hidden_errors = Matrix.Multiply(weight_ho_T, output_errors);
+
+            //gradient 계산
+            Matrix hidden_gradient = Matrix.map(hidden, this.activation_function.dfunc);
+            hidden_gradient.Multiply(hidden_errors);
+            hidden_gradient.Multiply(this.learning_rate);
+
+            //DELTA계산 input->hidden
+            Matrix inputs_T = Matrix.Transpose(inputs);
+            Matrix weight_ih_deltas = Matrix.Multiply(hidden_gradient, inputs_T);
+
+            this.weights_ih.Add(weight_ih_deltas);
+            this.bias_h.Add(hidden_gradient);
+
+        }
+
+        public NeuralNetwork Copy()
+        {
+            return new NeuralNetwork(this);
+        }
+
+        public void Mutate(Func<double, int, int, double> func)
+        {
+            this.weights_ih.map(func);
+            this.weights_ho.map(func);
+            this.bias_h.map(func);
+            this.bias_o.map(func);
+        }
+    }
+}
